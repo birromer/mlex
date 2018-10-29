a47c550a384890d4d227dc09f51c9b011f2b009d
birromer <hummescmpa@gmail.com>
2018-10-29 18:40:46 -0300

    added java project structure

---
+++ src/mlex/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mlex;
+
+/**
+ * @author birromer
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
