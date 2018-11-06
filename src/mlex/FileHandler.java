4a8d16239a6c628f9e912a48830e0c88136638c3
birromer <hummescmpa@gmail.com>
2018-11-06 09:01:12 -0200

    started filehandler

--- src/mlex/FileHandler.java
+++ src/mlex/FileHandler.java
@@ -1,8 +1,38 @@
 package mlex;
 
-public class FileHandler {
-	public void buscaArquivo()
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+abstract public class FileHandler
+{
+	String path = "./etc/";
+	String extensao = ".txt";
+	
+	public File abreArquivo(int nome)
 	{
-		
+		File arquivo;
+		try
+		{
+			arquivo =  new File(path + nome + extensao);
+		}
+		catch (FileNotFoundException e) 
+		{
+		    e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+		    e.printStackTrace();
+		}
+		finally
+		{
+			return arquivo;
+		}
 	}
+
+	
+	abstract public File buscaArquivo(int nome, int op);
+	
+	abstract public void atualizaArquivo(int nome, int op);
+	
 }
