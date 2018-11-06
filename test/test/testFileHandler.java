3c7e449bb44710c5c2e3a0463a9622ea45d9e532
birromer <hummescmpa@gmail.com>
2018-11-06 10:20:37 -0200

    added tests for file handler and index handler

---
+++ test/test/testFileHandler.java
@@ -0,0 +1,39 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import mlex.Jogo;
+
+public class testFileHandler
+{
+	Jogo jogoTeste;
+	
+	@Before
+	public void setUp()
+	{
+		jogoTeste = new Jogo(0, "Jogo Legal", "01/09/94", "Klei");
+		File arquivoTeste = new File("./etc/teste");
+	}
+
+	
+	@Test
+	public void testVerificaArquivo()
+	{
+		assertFalse(Jogo.verificaArquivo(jogoTeste.getIdJogo()));
+		//adiciona arquivo
+		//testa de novo
+	}
+	
+	
+	@Test
+	public void testEscreveArquivo()
+	{
+		
+	}
+
+}
---
+++ test/test/testIndice.java
@@ -0,0 +1,30 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import mlex.Indice;
+
+public class testIndice {
+	Indice indiceTeste;
+	
+	@Before
+	public void setUp()
+	{
+		indiceTeste = new Indice();
+	}
+
+	@Test
+	public void testAdicionaJogo()
+	{
+		
+	}
+	
+	public void testeModificaJogo()
+	{
+		
+	}
+
+}
