c95bc69d1a80e32aced4f036fe31b86af1e464fb
birromer <hummescmpa@gmail.com>
2018-11-13 16:12:48 -0200

    chama menu iniciar

--- src/mlex/Main.java
+++ src/mlex/Main.java
@@ -1,14 +1,21 @@
 package mlex;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main
 {
-	private static java.io.Console cnsl = System.console();
+	private static UsuarioCommand usuario = new UsuarioCommand();
+	private static Scanner scanner = new Scanner(System.in);
 	
 	public static void main(String[] args)
 	{
-
+		int opcaoMenu = -1;
+		do
+		{
+			usuario.menuInicial(opcaoMenu);
+			opcaoMenu = scanner.nextInt();
+		} while (opcaoMenu != 666);
 	}
 
 }
