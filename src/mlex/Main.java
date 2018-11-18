99d181ae9bfee6d786f6826a072b4af3f175a25d
birromer <hummescmpa@gmail.com>
2018-11-18 12:21:00 -0200

    added directories creation

--- src/mlex/Main.java
+++ src/mlex/Main.java
@@ -1,5 +1,6 @@
 package mlex;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main
@@ -9,12 +10,29 @@ public class Main
 	
 	public static void main(String[] args)
 	{
+		criaDiretorios();
+		
 		int opcaoMenu = -1;
 		do
 		{
 			usuario.menuInicial(opcaoMenu);
 			opcaoMenu = scanner.nextInt();
 		} while (opcaoMenu != 666);
+	}
+	
+	public static void criaDiretorios()
+	{
+		File diretorio1 = new File("./etc/");
+		File diretorio2 = new File("./etc/jogos/");
+		
+		if (diretorio1.exists() == false)
+		{
+			diretorio1.mkdir();
+			if (diretorio2.exists() == false)
+			{
+				diretorio2.mkdir();
+			}
+		}
 	}
 
 }
